c5039bcdafcf2a91708e20fd58d7cd55c2bd2dd7
i044312 <arpit.goyal@sap.com>
2020-11-21 15:38:17 -0800

    Fixing names

--- setup-images.sh
+++ setup-images.sh
@@ -3,4 +3,4 @@
 docker pull nginx:1.19.4
 kind load docker-image nginx:1.19.4
 ./3-the-metrics/metrics-image-setup.sh
-./4-consule-service-mesh/consule-image-setup.sh
+./4-consul-service-mesh/consul-image-setup.sh
