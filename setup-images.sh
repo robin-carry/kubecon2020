7156dea9a5eec69a2cec1e6727c29b3dbd377e01
i044312 <arpit.goyal@sap.com>
2020-11-21 15:43:20 -0800

    Fixing paths

--- setup-images.sh
+++ setup-images.sh
@@ -2,5 +2,7 @@
 
 docker pull nginx:1.19.4
 kind load docker-image nginx:1.19.4
+docker pull jettech/kube-webhook-certgen:v1.3.0
+kind load docker-image jettech/kube-webhook-certgen:v1.3.0
 ./3-the-metrics/metrics-image-setup.sh
 ./4-consul-service-mesh/consul-image-setup.sh
